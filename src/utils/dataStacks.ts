cba31338fbca97d8ddb90477f4b0e721068aa56c
Hugo Montaño <mobile.developer.mx@gmail.com>
2026-03-13 22:18:57 -0600

    Add Hero section with buttons and stacks display; implement button and stacks components with styles

---
+++ src/utils/dataStacks.ts
@@ -0,0 +1,14 @@
+export const heroStacks = [
+  {
+    stacks: "+10k",
+    text: "Equipos reparados",
+  },
+  {
+    stacks: "3M",
+    text: "Garantía real",
+  },
+  {
+    stacks: "24h",
+    text: "Diagnóstico rápido",
+  },
+];
